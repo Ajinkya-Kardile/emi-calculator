467e1a5bc80ab65ca60a6920c227f54918e68db5
Ajinkya <kardileajinkya@gmail.com>
2025-05-12 10:11:40 +0530

    **fixes**: favicon fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,10 +63,6 @@ export const metadata: Metadata = {
             {url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png"},
             {url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png"},
         ],
-        // shortcut: [
-        //     {url: "/favicon.ico", type: "image/x-icon"},
-        //     {url: "/favicon.svg", type: "image/svg+xml"},
-        // ],
         apple: [
             {url: "/apple-touch-icon.png", sizes: "180x180"}
         ]
